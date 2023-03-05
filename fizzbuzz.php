565d6025a2478a03a6f1eadcd3f15155fb310e9f
Moyinoluwalogo <m.oladipsgmail.com>
2023-03-05 20:40:48 +0100

    Initial Commit

---
+++ fizzbuzz.php
@@ -0,0 +1,20 @@
+<?php
+    function fizzbuzz($num) {
+        for ($i=1; $i < $num+1; $i++) { 
+            if ($i % 3 == 0 && $i % 5 == 0) {
+                echo "FizzBuzz <br>";
+            }
+            elseif ($i % 3 == 0) {
+                echo "Fizz <br>";
+            }
+            elseif ($i % 5 == 0) {
+                echo "Buzz <br>";
+            }
+            else {
+                echo "$i <br>";
+            }
+        }  
+    }
+
+fizzbuzz(15);
+?>
